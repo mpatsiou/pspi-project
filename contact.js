c52b164ccf7e0083e597e1ea268cf9636fc7dfbc
VasilisPits <pits_bill@hotmail.com>
2020-04-16 17:51:47 +0300

    Try to implement footer

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 
-  function check() {
-    var textarea = document.getElementById("inputPassword4");
-    if (textarea.value.length == 0) {
-      alert("You have to wright a comment");
-    }
+function check() {
+  var textarea = document.getElementById("inputPassword4");
+  if (textarea.value.length == 0) {
+    alert("You have to wright a comment");
   }
+}
