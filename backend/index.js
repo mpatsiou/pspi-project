36c72d5c9fa48148e212598e2a6c902173fb597f
mpatsiou <batsioularas@gmail.com>
2020-06-02 00:06:06 +0300

    Set up server

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const Routers = require('./routers/index.js')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use('/user', Routers.user)
+app.use('/post', Routers.post)
+app.use('/comment', Routers.comment)
+app.use('/session', Routers.session)
+app.use('/friendship', Routers.friendship)
+
+app.listen(3000)
